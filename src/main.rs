d89dfc87d6dc0b7ce761864a79d31edf79696362
Eddie.christian <eddiechristian@zoho.com>
2021-05-11 22:01:42 -0400

    added looping

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,33 @@ mod gpio;
 use crate::system::DeviceInfo;
 use crate::gpio::mem::{GpioMem, PATH_DEV_GPIOMEM };
 use crate::gpio::pin::{OutputPin, Pin};
+
 use std::rc::Rc;
+use std::thread;
+use std::time::Duration;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
 
 fn main() {
+    let running = Arc::new(AtomicBool::new(true));
+    let r = running.clone();
+
+    ctrlc::set_handler(move || {
+        r.store(false, Ordering::SeqCst);
+    }).expect("Error setting Ctrl-C handler");
+
     let dev_info = DeviceInfo::new();
     println!("{}", dev_info.unwrap());
     if let Ok(gpio_mem) =GpioMem::open(){
         println!("gpio_mem: {}", gpio_mem);
         let gpio_rc = Rc::new(gpio_mem);
-        let out_pin5 = Pin::new(27,gpio_rc).into_output();
+        let mut out_pin5 = Pin::new(27,gpio_rc).into_output();
+        while  running.load(Ordering::SeqCst){
+            out_pin5.set_high();
+            thread::sleep(Duration::from_millis(2000));
+            out_pin5.set_low();
+        }
+
     } else {
         println!("ERROR: unable to map {:?}",PATH_DEV_GPIOMEM);
     }
